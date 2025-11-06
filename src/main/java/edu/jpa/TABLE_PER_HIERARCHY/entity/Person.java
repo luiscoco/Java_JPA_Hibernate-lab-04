63fe72f70a3aa446d607402320075a66591feaba
Luis Coco Enríquez <LEnriquez@luxoft.com>
2025-11-06 15:43:21 +0100

    first commit

---
+++ src/main/java/edu/jpa/TABLE_PER_HIERARCHY/entity/Person.java
@@ -0,0 +1,32 @@
+package edu.jpa.TABLE_PER_HIERARCHY.entity;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Person {
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
